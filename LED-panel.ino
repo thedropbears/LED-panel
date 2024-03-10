517f694e8251f6e0b98daa74ec4e5eab0047c742
Seb-TheDropBears <seb.zanardo@thedropbears.org.au>
2024-03-10 17:26:03 +1100

    Added pre_match animations

--- LED-panel.ino
+++ LED-panel.ino
@@ -93,7 +93,7 @@ Animation create_animation(int frames_len, Frame *frames) {
     return new_animation;
 }
 
-///////////////////////////////////////////////////////////////////////////////
+// MATCH ANIMATIONS ///////////////////////////////////////////////////////////
 
 void eyes_static(int ticks) {
     matrix->fillCircle(EYE_L_X, EYE_L_Y, EYE_RADIUS, WHITE);
@@ -139,70 +139,69 @@ void eyes_blink(int ticks) {
 
 // Duration=2
 void eyes_angry(int ticks) {
-
     int pupil_offset;
-  if (ticks % 2 == 0) {
-    pupil_offset = 1;
-  } else {
-    pupil_offset = 0;
-  }
-  matrix->fillCircle(EYE_L_X, EYE_L_Y, EYE_RADIUS, WHITE);
-  matrix->fillCircle(EYE_R_X, EYE_R_Y, EYE_RADIUS, WHITE);
-  matrix->fillCircle(EYE_R_X + pupil_offset, EYE_R_Y+4, PUPIL_RADIUS+2, BLACK);
-  matrix->fillCircle(EYE_L_X - pupil_offset, EYE_L_Y+4, PUPIL_RADIUS+2, BLACK);
-  matrix->fillTriangle(EYE_L_X-EYE_RADIUS, 0, EYE_L_X+EYE_RADIUS, 0, EYE_L_X+EYE_RADIUS, EYE_L_Y, BLACK);
-  matrix->fillTriangle(EYE_R_X-EYE_RADIUS, 0, EYE_R_X+EYE_RADIUS, 0, EYE_R_X-EYE_RADIUS, EYE_R_Y, BLACK);
-
+    if (ticks % 2 == 0) {
+        pupil_offset = 1;
+    } else {
+        pupil_offset = 0;
+    }
+    matrix->fillCircle(EYE_L_X, EYE_L_Y, EYE_RADIUS, WHITE);
+    matrix->fillCircle(EYE_R_X, EYE_R_Y, EYE_RADIUS, WHITE);
+    matrix->fillCircle(EYE_R_X + pupil_offset, EYE_R_Y+4, PUPIL_RADIUS+2, BLACK);
+    matrix->fillCircle(EYE_L_X - pupil_offset, EYE_L_Y+4, PUPIL_RADIUS+2, BLACK);
+    matrix->fillTriangle(EYE_L_X-EYE_RADIUS, 0, EYE_L_X+EYE_RADIUS, 0, EYE_L_X+EYE_RADIUS, EYE_L_Y, BLACK);
+    matrix->fillTriangle(EYE_R_X-EYE_RADIUS, 0, EYE_R_X+EYE_RADIUS, 0, EYE_R_X-EYE_RADIUS, EYE_R_Y, BLACK);
 }
 
 // Duration=12
 void eyes_sad(int ticks) {
-  int tear_offset;
-  if ((ticks % 12) >= 6) {
-    tear_offset = 0;
-  } else {
-    tear_offset = 1;
-  }
-  matrix->fillCircle(EYE_L_X, EYE_L_Y, EYE_RADIUS, WHITE);
-  matrix->fillCircle(EYE_R_X, EYE_R_Y, EYE_RADIUS, WHITE);
-  matrix->fillCircle(EYE_R_X, EYE_R_Y+4, PUPIL_RADIUS+2, BLACK);
-  matrix->fillCircle(EYE_L_X, EYE_L_Y+4, PUPIL_RADIUS+2, BLACK);
-  matrix->fillTriangle(EYE_L_X-EYE_RADIUS, 0, EYE_L_X+EYE_RADIUS, 0, EYE_L_X-EYE_RADIUS, EYE_L_Y, BLACK);
-  matrix->fillTriangle(EYE_R_X-EYE_RADIUS, 0, EYE_R_X+EYE_RADIUS, 0, EYE_R_X+EYE_RADIUS, EYE_R_Y, BLACK);
-  matrix->fillCircle(EYE_L_X+2+tear_offset, EYE_L_Y+8, 2, CYAN);
-  matrix->fillCircle(EYE_R_X-2-tear_offset, EYE_R_Y+8, 2, CYAN);
-  matrix->fillCircle(EYE_L_X-1-tear_offset, EYE_L_Y+8, 2, CYAN);
-  matrix->fillCircle(EYE_R_X+1+tear_offset, EYE_R_Y+8, 2, CYAN);
+    int tear_offset;
+    if ((ticks % 12) >= 6) {
+        tear_offset = 0;
+    } else {
+        tear_offset = 1;
+    }
+    matrix->fillCircle(EYE_L_X, EYE_L_Y, EYE_RADIUS, WHITE);
+    matrix->fillCircle(EYE_R_X, EYE_R_Y, EYE_RADIUS, WHITE);
+    matrix->fillCircle(EYE_R_X, EYE_R_Y+4, PUPIL_RADIUS+2, BLACK);
+    matrix->fillCircle(EYE_L_X, EYE_L_Y+4, PUPIL_RADIUS+2, BLACK);
+    matrix->fillTriangle(EYE_L_X-EYE_RADIUS, 0, EYE_L_X+EYE_RADIUS, 0, EYE_L_X-EYE_RADIUS, EYE_L_Y, BLACK);
+    matrix->fillTriangle(EYE_R_X-EYE_RADIUS, 0, EYE_R_X+EYE_RADIUS, 0, EYE_R_X+EYE_RADIUS, EYE_R_Y, BLACK);
+    matrix->fillCircle(EYE_L_X+2+tear_offset, EYE_L_Y+8, 2, CYAN);
+    matrix->fillCircle(EYE_R_X-2-tear_offset, EYE_R_Y+8, 2, CYAN);
+    matrix->fillCircle(EYE_L_X-1-tear_offset, EYE_L_Y+8, 2, CYAN);
+    matrix->fillCircle(EYE_R_X+1+tear_offset, EYE_R_Y+8, 2, CYAN);
 }
 
 // Duration=6
 void eyes_aiming(int ticks) {
-  int pupil_offset;
-  if ((ticks % 6) >= 3) {
-    pupil_offset = -1;
-  } else {
-    pupil_offset = 1;
-  }
-  matrix->fillCircle(EYE_R_X, EYE_R_Y, EYE_RADIUS, WHITE);
-  matrix->fillCircle(EYE_L_X, EYE_L_Y, EYE_RADIUS, WHITE);
-  //matrix->fillTriangle(EYE_R_X-EYE_RADIUS, 0, EYE_R_X+EYE_RADIUS, 0, EYE_R_X-EYE_RADIUS, EYE_R_Y, BLACK);
-  matrix->fillCircle(EYE_R_X+pupil_offset, EYE_R_Y-4, PUPIL_RADIUS+3, BLACK);
-  matrix->fillCircle(EYE_L_X+pupil_offset, EYE_L_Y-4, PUPIL_RADIUS+3, BLACK);
-  matrix->fillRect(0,10,WIDTH,HEIGHT,BLACK);
+    int pupil_offset;
+    if ((ticks % 6) >= 3) {
+        pupil_offset = -1;
+    } else {
+        pupil_offset = 1;
+    }
+    matrix->fillCircle(EYE_R_X, EYE_R_Y, EYE_RADIUS, WHITE);
+    matrix->fillCircle(EYE_L_X, EYE_L_Y, EYE_RADIUS, WHITE);
+    //matrix->fillTriangle(EYE_R_X-EYE_RADIUS, 0, EYE_R_X+EYE_RADIUS, 0, EYE_R_X-EYE_RADIUS, EYE_R_Y, BLACK);
+    matrix->fillCircle(EYE_R_X+pupil_offset, EYE_R_Y-4, PUPIL_RADIUS+3, BLACK);
+    matrix->fillCircle(EYE_L_X+pupil_offset, EYE_L_Y-4, PUPIL_RADIUS+3, BLACK);
+    matrix->fillRect(0,10,WIDTH,HEIGHT,BLACK);
 
 }
 // Duration=24
 void eyes_rolling(int ticks) {
-  int time_ref = ticks % 24;
-  int radius = 5;
-  float desired_angle = time_ref * 2*3.14 / 24;
-  int x_offset = round(5 * cos(desired_angle));
-  int y_offset = round(5 * sin(desired_angle));
-  matrix->fillCircle(EYE_L_X, EYE_L_Y, EYE_RADIUS, WHITE);
-  matrix->fillCircle(EYE_R_X, EYE_R_Y, EYE_RADIUS, WHITE);
-  matrix->fillCircle(EYE_L_X+x_offset, EYE_L_Y+y_offset, PUPIL_RADIUS, BLACK);
-  matrix->fillCircle(EYE_R_X-x_offset, EYE_R_Y+y_offset, PUPIL_RADIUS, BLACK);
+    int time_ref = ticks % 24;
+    int radius = 5;
+    float desired_angle = time_ref * 2*3.14 / 24;
+    int x_offset = round(5 * cos(desired_angle));
+    int y_offset = round(5 * sin(desired_angle));
+    matrix->fillCircle(EYE_L_X, EYE_L_Y, EYE_RADIUS, WHITE);
+    matrix->fillCircle(EYE_R_X, EYE_R_Y, EYE_RADIUS, WHITE);
+    matrix->fillCircle(EYE_L_X+x_offset, EYE_L_Y+y_offset, PUPIL_RADIUS, BLACK);
+    matrix->fillCircle(EYE_R_X-x_offset, EYE_R_Y+y_offset, PUPIL_RADIUS, BLACK);
 }
+
 ///////////////////////////////////////////////////////////////////////////////
 
 int read_serial_port(uint8_t *packet) {
@@ -229,7 +228,6 @@ void render_status_flashing(int colour, int ticks) {
         matrix->fillRect(WIDTH-STATUS_WIDTH, 0, STATUS_WIDTH, HEIGHT, BLACK);
     }
 }
-
 
 // Create animation frames
 const int eye_frames_all_len = 4;
@@ -274,14 +272,189 @@ Frame eye_frames_extending[eye_frames_extending_len] = {
 
 // Create animations
 int animation_index = 0;
-const int animations_len = 6;
-Animation animations[animations_len] = {
+const int match_animations_len = 6;
+Animation match_animations[match_animations_len] = {
     create_animation(eye_frames_all_len, eye_frames_all),
     create_animation(eye_frames_intake_len, eye_frames_intake),
     create_animation(eye_frames_idle_len, eye_frames_idle),
     create_animation(eye_frames_out_range_len, eye_frames_out_range),
     create_animation(eye_frames_in_range_len, eye_frames_in_range),
     create_animation(eye_frames_extending_len, eye_frames_extending),
+};
+
+// PRE_MATCH ANIMATIONS ///////////////////////////////////////////////////////////
+
+void team_logo(int ticks) {
+    matrix->drawRGBBitmap(0, 0, (const uint16_t*)dblogo, 39, 16);
+    matrix->setCursor(40, 0);
+    matrix->println("DROP");
+    matrix->setCursor(40, 9);
+    matrix->println("BEARS");
+}
+
+void scroll_text(int ticks, String text) {
+    matrix->setCursor(WIDTH - ticks, HEIGHT);
+    matrix->println(text);
+}
+
+void match1(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match2(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match3(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match4(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match5(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match6(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match7(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match8(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match9(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match10(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match11(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match12(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match13(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match14(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+void match15(int ticks) {
+    scroll_text(ticks, "4774: The Drop Bears        ????: Unknown Team        ????: Unknown Team");
+}
+
+const int practice_len = 1;
+Frame practice[practice_len] = {
+    create_frame(&team_logo, 1)
+};
+
+const int match1_len = 1;
+Frame match1_frames[match1_len] = {
+    create_frame(&match1, 100)
+};
+
+const int match2_len = 1;
+Frame match2_frames[match2_len] = {
+    create_frame(&match2, 100)
+};
+
+const int match3_len = 1;
+Frame match3_frames[match3_len] = {
+    create_frame(&match3, 100)
+};
+
+const int match4_len = 1;
+Frame match4_frames[match4_len] = {
+    create_frame(&match4, 100)
+};
+
+const int match5_len = 1;
+Frame match5_frames[match5_len] = {
+    create_frame(&match5, 100)
+};
+
+const int match6_len = 1;
+Frame match6_frames[match6_len] = {
+    create_frame(&match6, 100)
+};
+
+const int match7_len = 1;
+Frame match7_frames[match7_len] = {
+    create_frame(&match7, 100)
+};
+
+const int match8_len = 1;
+Frame match8_frames[match8_len] = {
+    create_frame(&match8, 100)
+};
+
+const int match9_len = 1;
+Frame match9_frames[match9_len] = {
+    create_frame(&match9, 100)
+};
+
+const int match10_len = 1;
+Frame match10_frames[match10_len] = {
+    create_frame(&match10, 100)
+};
+
+const int match11_len = 1;
+Frame match11_frames[match11_len] = {
+    create_frame(&match11, 100)
+};
+
+const int match12_len = 1;
+Frame match12_frames[match12_len] = {
+    create_frame(&match12, 100)
+};
+
+const int match13_len = 1;
+Frame match13_frames[match13_len] = {
+    create_frame(&match13, 100)
+};
+
+const int match14_len = 1;
+Frame match14_frames[match14_len] = {
+    create_frame(&match14, 100)
+};
+
+const int match15_len = 1;
+Frame match15_frames[match15_len] = {
+    create_frame(&match15, 100)
+};
+
+const int pre_match_animations_len = 16;
+Animation pre_match_animations[pre_match_animations_len] = {
+    create_animation(practice_len, practice),
+    create_animation(match1_len, match1_frames),
+    create_animation(match2_len, match2_frames),
+    create_animation(match3_len, match3_frames),
+    create_animation(match4_len, match4_frames),
+    create_animation(match5_len, match5_frames),
+    create_animation(match6_len, match6_frames),
+    create_animation(match7_len, match7_frames),
+    create_animation(match8_len, match8_frames),
+    create_animation(match9_len, match9_frames),
+    create_animation(match10_len, match10_frames),
+    create_animation(match11_len, match11_frames),
+    create_animation(match12_len, match12_frames),
+    create_animation(match13_len, match13_frames),
+    create_animation(match14_len, match14_frames),
+    create_animation(match15_len, match15_frames)
 };
 
 uint8_t packet = 0;
@@ -295,42 +468,48 @@ void setup() {
     matrix->begin();
     matrix->setTextWrap(false);
     matrix->setBrightness(BRIGHTNESS);
+    matrix->setFont(&FreeSerifBoldItalic9pt7b);
 
     Serial.begin(9600);
 }
 
+uint8_t current_match_state;
 uint8_t status_lights;
-uint8_t match_state;
 uint8_t match_id;
 
 void loop() {
     // INPUT
     if (read_serial_port(&packet)) {
-      // Unpack byte
-      uint8_t payload = (packet >> 0) & 0b111111;
-      match_state =     (packet >> 6) & 0b11;
+        // Unpack byte
+        uint8_t payload = (packet >> 0) & 0b111111;
+        uint8_t match_state = (packet >> 6) & 0b11;
 
-      status_lights =   (payload >> 0) & 0b111;
-      match_id =        (payload >> 0) & 0b1111;
+        status_lights = (payload >> 0) & 0b111;
+        match_id = (payload >> 0) & 0b1111;
 
-      Serial.print("received byte ");
-      Serial.println(payload);
-      Serial.print("Current status ");
-      Serial.println(status_lights);
+        if (current_match_state != match_state) {
+            animation_index = 0;
+            current_match_state = match_state;
+        }
 
-      switch (status_lights) {
-      case 4:
-          high_priority_pattern = CLIMB_EXTENDING;
-          break;
-      case 5:
-          high_priority_pattern = CLIMB_EXTENDED;
-          break;
-      case 6:
-          high_priority_pattern = CLIMB_RETRACTED;
-          break;
-      default:
-          pattern = status_lights;
-      }
+        Serial.print("received byte ");
+        Serial.println(payload);
+        Serial.print("Current status ");
+        Serial.println(status_lights);
+
+        switch (status_lights) {
+        case 4:
+            high_priority_pattern = CLIMB_EXTENDING;
+            break;
+        case 5:
+            high_priority_pattern = CLIMB_EXTENDED;
+            break;
+        case 6:
+            high_priority_pattern = CLIMB_RETRACTED;
+            break;
+        default:
+            pattern = status_lights;
+        }
     }
 
     // UPDATE
@@ -339,8 +518,7 @@ void loop() {
     // RENDER
     matrix->clear();
 
-    // Draw current animation's current frame
-    Animation* animation = &animations[animation_index];
+    Animation* animation = &match_animations[animation_index];
     Frame frame = animation->frames[animation->frame_index];
     frame.render(animation->ticks);
     animation->ticks++;
@@ -352,44 +530,47 @@ void loop() {
         animation->ticks = 0;
     }
 
-    // Draw current status as bars over animation
-        if (high_priority_pattern == CLIMB_RETRACTED) {
-        switch (pattern){
-            case EMPTY:
-                render_status(WHITE);
-                animation_index = 2;
-                break;
-            case INTAKE:
-                render_status(MAGENTA);
-                animation_index = 1;
-                break;
-            case IN_RANGE:
-                render_status(GREEN);
-                animation_index = 4;
-                break;
-            case NOT_IN_RANGE:
-                render_status(RED);
-                animation_index = 3;
-                break;
-            case OFF:
-                render_status(BLACK);
-                animation_index = 2;
-                break;
+    // Draw current animation's current frame
+    if (current_match_state == 0) {
+        // Draw current status as bars over animation
+            if (high_priority_pattern == CLIMB_RETRACTED) {
+            switch (pattern){
+                case EMPTY:
+                    render_status(BLACK);
+                    animation_index = 2;
+                    break;
+                case INTAKE:
+                    render_status(MAGENTA);
+                    animation_index = 1;
+                    break;
+                case IN_RANGE:
+                    render_status(GREEN);
+                    animation_index = 4;
+                    break;
+                case NOT_IN_RANGE:
+                    render_status(RED);
+                    animation_index = 3;
+                    break;
+                case OFF:
+                    render_status(BLACK);
+                    animation_index = 2;
+                    break;
+            }
+        }
+        else {
+            switch (high_priority_pattern){
+                case CLIMB_EXTENDING:
+                    render_status_flashing(YELLOW, global_ticks);
+                    animation_index = 5;
+                    break;
+                case CLIMB_EXTENDED:
+                    render_status(YELLOW);
+                    animation_index = 2;
+                    break;
+            }
         }
     }
-    else {
-        switch (high_priority_pattern){
-            case CLIMB_EXTENDING:
-                render_status_flashing(YELLOW, global_ticks);
-                animation_index = 5;
-                break;
-            case CLIMB_EXTENDED:
-                render_status(YELLOW);
-                animation_index = 2;
-                break;
-        }
-    }
-
+    
     delay(1);
     matrix->show();
 }
